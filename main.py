80ceb0587e15c8527888de3168c8b21608cb2dad
FamsFamily <145118246+FamsFamily@users.noreply.github.com>
2024-01-18 21:28:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 meme_dict = {
-            "gaje": "gak jelas",
+            "gaje": "GAk JElas",
             "gabut": "gak ada kerjaan",
-            "ygy": "ya guys ya"
-            
+            "ygy": "Ya Guys Ya",
+            "cmiiw": "Correct Me If I'm Wrong (koreksi saya jika saya salah)",
+            "gercep": "GERak CEPat",
+            "mager": "MAles GERak",
+            "mantul": "MANtap beTUL",
+            "jadul": "JAman DULu",
+            "anw": "anyway (ngomong-ngomong)",
+            "btw": "By The Way (ngomong-ngomong)",
+            "aka": "As Known As (Alias)",
+            "otw": "On The Way (dalam perjalanan)",
+            "lol": "Laugh Of Loud (tertawa keras)",
+            "gws": "Get Well Soon (semoga lekas sembuh)"
             }
             
             
-for i in range(5):
+for i in range(len(meme_dict):
     word = input("Ketik kata yang tidak Kamu mengerti: ")
     if word in meme_dict.keys():
         print(meme_dict[word])
